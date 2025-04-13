fef814937f28412eeb8edd492149dcb993597555
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-04-13 13:41:24 -0400

    Update __init__.py

--- jupyter_templates/__init__.py
+++ jupyter_templates/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< before updating
 # *****************************************************************************
 #
 # Copyright (c) 2024, the jupyter_templates authors.
@@ -18,35 +17,3 @@ def _jupyter_server_extension_paths():
 
 def _jupyter_server_extension_points():
     return [{"module": "jupyter_templates.extension"}]
-=======
-__version__ = "0.1.0"
-
-
-def _jupyter_server_extension_paths():
-    return [{"module": "jupyter_templates"}]
-
-
-def _jupyter_server_extension_points():
-    return [{"module": "jupyter_templates"}]
-
-
-def _load_jupyter_server_extension(nb_server_app, nb6_entrypoint=False):
-    """
-    Called when the extension is loaded.
-
-    Args:
-        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
-    """
-    # web_app = nb_server_app.web_app
-
-
-def _jupyter_nbextension_paths():
-    return [
-        {
-            "section": "tree",
-            "src": "nbextension/static",
-            "dest": "jupyter_templates",
-            "require": "jupyter_templates/notebook",
-        }
-    ]
->>>>>>> after updating
